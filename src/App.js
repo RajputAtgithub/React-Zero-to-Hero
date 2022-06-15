4aff27b92058b6bc2c4515fae245ad8a3f7361ae
luv-jeri <luvvjeri@gmail.com>
2022-06-15 21:16:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Card from './card';
 function App() {
+  const data = [
+    {
+      id: 1,
+      name: 'John',
+      age: '20',
+      email: 'john@gmail.com',
+      job: 'teacher',
+    },
+    {
+      id: 2,
+      name: 'Peter',
+      age: '30',
+      email: 'peter@gmail.com',
+      job: 'student',
+    },
+    {
+      id: 3,
+      name: 'Jane',
+      age: '40',
+      email: 'jane@gmail.co,',
+      job: 'designer',
+    },
+    {
+      id: 4,
+      name: 'Jane',
+      age: '40',
+      email: 'jane@gmail.co,',
+      job: 'designer',
+    },
+    {
+      id: 4,
+      name: 'Sanjay',
+      age: '22',
+      email: 'sanjay@gmail.co,',
+      job: 'Software Engineer',
+    },
+  ];
+
+  function loop(obj) {
+    return Card(obj);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {data.map(loop)}
+      {/* <h1 className='heading'>Count {count}</h1>
+      <button onClick={clickFunction}>Increase </button> */}
     </div>
   );
 }
